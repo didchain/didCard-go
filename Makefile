36f764d0fcf1f8e2bca5817f5c6b14727cc5bf43
stephenliao-tutan <stephen.liao@protonmail.com>
2021-01-29 14:14:56 +0800

    add to repo

--- Makefile
+++ Makefile
@@ -3,7 +3,8 @@ BINDIR=bin
 all: i a
 
 a:
-	gomobile bind -v -o $(BINDIR)/dss.aar -target=android github.com/didchain/didCard-go/android
+	gomobile bind -v -x -o $(BINDIR)/dss.aar -target=android github.com/didchain/didCard-go/android
+	#gomobile bind -v -x -a ../../bls-eth-go-binary/android/obj/local/armeabi-v7a/libbls384_256.a -o $(BINDIR)/dss.aar -target=android github.com/didchain/didCard-go/android
 i:
 	gomobile bind -v -o $(BINDIR)/iosLib.framework -target=ios github.com/didchain/didCard-go/ios
 
--- android/interface.go
+++ android/interface.go
@@ -1,1 +1,85 @@
 package android
+
+import (
+	"encoding/json"
+	"github.com/didchain/didCard-go/card"
+)
+
+var _cardInst card.DIDCard = nil
+
+func NewCard(auth string) []byte {
+	card, err := card.NewSimpleCard(auth)
+	if err != nil {
+		return nil
+	}
+
+	encodedFile, err := json.MarshalIndent(card, "", "\t")
+	if err != nil {
+		return nil
+	}
+
+	_cardInst = card
+	return encodedFile
+}
+
+func LoadCard(jsonStr string) bool {
+	card, err := card.ParseCard(jsonStr)
+	if err != nil {
+		return false
+	}
+	_cardInst = card
+	return true
+}
+
+func LoadCardByPath(fullPath string) bool {
+	card, err := card.LoadCard(fullPath)
+	if err != nil {
+		return false
+	}
+	_cardInst = card
+	return true
+}
+
+func Import(auth, jsonStr string) []byte {
+	card, err := card.ParseCard(jsonStr)
+	if err != nil {
+		return nil
+	}
+	if !card.Open(auth) {
+		return nil
+	}
+	_cardInst = card
+	return []byte(card.PublicKey())
+}
+
+func Open(auth string) bool {
+	if _cardInst == nil {
+		return false
+	}
+
+	return _cardInst.Open(auth)
+}
+
+func IsOpen() bool {
+	if _cardInst == nil {
+		return false
+	}
+
+	return _cardInst.IsOpen()
+}
+
+func SignByPassword(msg, auth string) []byte {
+	if _cardInst == nil {
+		return nil
+	}
+	_cardInst.Open(auth)
+	return _cardInst.Sign([]byte(msg))
+}
+
+func Sign(msg string) []byte {
+	return _cardInst.Sign([]byte(msg))
+}
+
+func Verify(pub, msg, sig []byte) bool {
+	return card.Verify(pub, sig, msg)
+}
